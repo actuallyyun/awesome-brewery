baac59e235d9c0857753465ab36cd530708f7f23
Yun Ji <this.jiyun@gmail.com>
2024-02-08 12:53:45 +0100

    modify hook to not just return data array

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,9 +1,9 @@
-import axios, { AxiosError, AxiosResponse } from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { useEffect, useState } from 'react'
 import { ApiError, NetworkError } from '../miscs/Error'
 
 export function useFetch<T>(url: string) {
-  const [data, setData] = useState<T[] | null>(null)
+  const [data, setData] = useState<T>()
   const [error, setError] = useState<ApiError | NetworkError | undefined>(
     undefined
   )
@@ -11,6 +11,7 @@ export function useFetch<T>(url: string) {
 
   useEffect(() => {
     async function startFetching(url: string) {
+      console.log('fetching')
       try {
         const response: AxiosResponse = await axios.get(url)
         if (response.status === 200 && !ignore) {
