a3cda27a7e17f92f7d15a927ee4ff9b649290d6e
Yun Ji <this.jiyun@gmail.com>
2024-02-08 13:48:39 +0100

    change card content

--- src/components/brewery/BreweryCard.tsx
+++ src/components/brewery/BreweryCard.tsx
@@ -1,35 +1,21 @@
 import { Link } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 import { BreweryCardProp } from '../../miscs/BreweryType'
-import { capitalizeFirstLetter, googleMapUrl } from '../../miscs/utils'
 
 export default function BreweryCard({ brewery }: BreweryCardProp): JSX.Element {
   return (
     <div>
       <h1>{brewery.name}</h1>
-      <Link to={`./breweries/${brewery.id}`}>
-        <button>lean more</button>
-      </Link>
       <p>{brewery.address_1}</p>
       {brewery.address_2 && <p>{brewery.address_2}</p>}
       {brewery.address_3 && <p>{brewery.address_2}</p>}
       <p>
         {brewery.city},{brewery.state} {brewery.postal_code}
       </p>
-      <p>{brewery.phone}</p>
-      <p>Type:{capitalizeFirstLetter(brewery.brewery_type)}</p>
-      <p>
-        <a href={brewery.website_url}>{brewery.website_url}</a>
-      </p>
-      {brewery.latitude && brewery.latitude && (
-        <a
-          href={googleMapUrl(brewery.latitude, brewery.longitude)}
-          target='_blank'
-          rel='noreferrer'
-        >
-          View Map
-        </a>
-      )}
+      <Link to={`./breweries/${brewery.id}`}>
+        <Button variant='contained'>Learn more</Button>
+      </Link>
     </div>
   )
 }
