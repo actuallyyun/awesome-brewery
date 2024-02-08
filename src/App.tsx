94be4fb5cff242218406c487a2bd458e79325e98
Yun Ji <this.jiyun@gmail.com>
2024-02-08 11:55:23 +0100

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import React, { useState } from 'react'
-import { Routes,Route } from 'react-router-dom'
+import React, { useState, useMemo } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Grid from '@mui/material/Unstable_Grid2'
 import Item from '@mui/material/Grid/'
 import Container from '@mui/material/Container'
 
 import { useFetch } from './hooks/useFetch'
-import { Brewery } from './miscs/Brewery'
+import { BreweryType } from './miscs/BreweryType'
 import BreweryCard from './components/brewery/BreweryCard'
 import Search from './components/search/Search'
 import { appendSearchUrl } from './miscs/utils'
 import Header from './components/header/Header'
+import Home from './pages/Home'
+import Admin from './pages/admin'
+import Brewery from './pages/Brewery'
 
 const LISTBREWERIERSURL = 'https://api.openbrewerydb.org/v1/breweries'
 
 const App = () => {
-  //const [searchName, setSearchName] = useState<string | undefined>(undefined)
   const [url, setUrl] = useState<string>(LISTBREWERIERSURL)
   console.log({ url })
 
   const [searchName, setSearchName] = useState('')
 
-  const handleNameSearch = (e: React.FormEvent) => {
-    const target = e.target as HTMLInputElement
-    setSearchName((n) => (n = target.value))
-    setUrl((url) => (url = appendSearchUrl(LISTBREWERIERSURL, target.value)))
+  const handleNameSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchName((n) => (n = e.target.value))
+    setUrl((url) => (url = appendSearchUrl(LISTBREWERIERSURL, e.target.value)))
   }
 
-  const { data, loading, error } = useFetch<Brewery>(url)
+  //const memoSearchName = useMemo(() => setSearchName(searchName), [searchName])
+
+  const { data, loading, error } = useFetch<BreweryType>(url)
   console.log({ data, loading, error })
   return (
     <Container maxWidth='md'>
@@ -38,19 +41,11 @@ const App = () => {
           <Search value={searchName} onChange={handleNameSearch} />
         </Grid>
       </Grid>
-      <Grid container spacing={2}>
-        <Grid xs={12}>{error && <p>oops something went wrong...</p>}</Grid>
-        {data.length > 0 &&
-          data.map((brewery, index) => {
-            return (
-              <Grid xs={6} md={4} key={index}>
-                <Item>
-                  <BreweryCard brewery={brewery} />
-                </Item>
-              </Grid>
-            )
-          })}
-      </Grid>
+      <Routes>
+        <Route path='/' element={<Home data={data} error={error} />}></Route>
+        <Route path='/brewery/:id' element={<Brewery />}></Route>
+        <Route path='/admin' element={<Admin />}></Route>
+      </Routes>
     </Container>
   )
 }
