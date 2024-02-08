20efccfefb5a1fc473b1f8281fcc0cf88a577702
Yun Ji <this.jiyun@gmail.com>
2024-02-08 15:52:16 +0100

    create select state component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,16 +1,30 @@
 import Input from '@mui/material/Input'
+import Grid from '@mui/material/Unstable_Grid2'
+import Item from '@mui/material/Grid'
 
 import { SearchProps } from '../../miscs/SearchType'
+import StateSelect from './SelectState'
 
 export default function Search({ value, onChange }: SearchProps): JSX.Element {
   return (
-    <Input
-      fullWidth
-      type='text'
-      value={value}
-      onChange={onChange}
-      placeholder='company name'
-      aria-label='title'
-    />
+    <Grid container spacing={2}>
+      <Grid xs={6}>
+        <Item>
+          <Input
+            fullWidth
+            type='text'
+            value={value}
+            onChange={onChange}
+            placeholder='company name'
+            aria-label='title'
+          />
+        </Item>
+      </Grid>
+      <Grid xs={3}>
+        <Item>
+          <StateSelect />
+        </Item>
+      </Grid>
+    </Grid>
   )
 }
